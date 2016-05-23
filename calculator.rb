881c32fec4dd6759b1b333effd82a13897fdaecc
Dafin <dafin@users.noreply.github.com>
2016-05-23 12:13:40 +0100

    Updating test cases

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,21 @@
 class Calculator
 	attr_accessor :accumalator
+
+def add(val)
+	accumalator += val
+end
+
+def multiply(val)
+		accumalator *= val
+end
+
+def subtract(val)
+		accumalator -= val
+end
+
+
+def divide(val)
+		accumalator /= val
+end
+
 end
--- calculator_spec.rb
+++ calculator_spec.rb
@@ -8,4 +8,36 @@ class CalculatorSpec < MiniTest::Test
 		my_calculator.accumalator = 5
 		assert_equal(my_calculator.accumalator, 5)
 	end
+
+
+	def test_add
+		my_calculator = Calculator.new
+		my_calculator.add = 5
+		assert_equal(12, my_calculator.add, 5)
+	end
+
+	def test_multiply
+		my_calculator = Calculator.new
+		my_calculator.accumalator = 5
+		assert_equal(my_calculator.multiply, 5)
+		my_calculator.multiply = 5(5,1)
+		end
+
+
+	def test_subtract
+		my_calculator = Calculator.new
+		my_calculator.accumalator = 5
+		my_calculator.subtract = 5(12,7)
+		assert_equal(my_calculator.subtract, 5)
+	end
+
+	def test_divide
+		my_calculator = Calculator.new
+		my_calculator.accumalator = 5
+		assert_equal(2.5, my_calculator.divide, (2))
+		my_calculator.divide = 2(2.5)
+
+	end
+
+
 end
