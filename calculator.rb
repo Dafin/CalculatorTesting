930746f3e0041905618032fb969ec8b0e4240ad6
Dafin <dafin@users.noreply.github.com>
2016-05-23 12:29:17 +0100

    Added a divide by 0 function

--- calculator.rb
+++ calculator.rb
@@ -1,21 +1,22 @@
 class Calculator
 	attr_accessor :accumalator
 
-def add(val)
-	accumalator += val
-end
+	def add(val)
+		accumalator += val
+	end
 
-def multiply(val)
-		accumalator *= val
-end
+	def multiply(val)
+			accumalator *= val
+	end
 
-def subtract(val)
-		accumalator -= val
-end
+	def subtract(val)
+			accumalator -= val
+	end
 
 
-def divide(val)
-		accumalator /= val
-end
+	def divide(val)
+		return nil if val == 0
+			accumalator /= val
+	end
 
 end
--- calculator_spec.rb
+++ calculator_spec.rb
@@ -2,42 +2,45 @@ require 'minitest/autorun'
 require './calculator'
 
 class CalculatorSpec < MiniTest::Test
-
-	def test_accumalator
-		my_calculator = Calculator.new
-		my_calculator.accumalator = 5
-		assert_equal(my_calculator.accumalator, 5)
-	end
-
-
-	def test_add
-		my_calculator = Calculator.new
-		my_calculator.add = 5
-		assert_equal(12, my_calculator.add, 5)
-	end
-
-	def test_multiply
-		my_calculator = Calculator.new
-		my_calculator.accumalator = 5
-		assert_equal(my_calculator.multiply, 5)
-		my_calculator.multiply = 5(5,1)
-		end
+  def test_accumulator
+    my_calculator = Calculator.new
+    my_calculator.accumulator = 5
+    assert_equal(my_calculator.accumulator, 5)
+  end
 
 
-	def test_subtract
-		my_calculator = Calculator.new
-		my_calculator.accumalator = 5
-		my_calculator.subtract = 5(12,7)
-		assert_equal(my_calculator.subtract, 5)
-	end
-
-	def test_divide
-		my_calculator = Calculator.new
-		my_calculator.accumalator = 5
-		assert_equal(2.5, my_calculator.divide, (2))
-		my_calculator.divide = 2(2.5)
-
-	end
+  def test_accumulator_is_floating_point
+    my_calculator = Calculator.new
+    my_calculator.accumulator = 5
+    assert_instance_of(Float, my_calculator.accumulator)
+  end
 
 
+  def test_add
+    my_calculator = Calculator.new
+    my_calculator.accumulator = 5
+    assert_equal(my_calculator.add(7), 12)
+    assert_equal(my_calculator.add(-12), 0)
+  end
+
+  def test_subtract
+    my_calculator = Calculator.new
+    my_calculator.accumulator = 5
+    assert_equal(my_calculator.subtract(7), -2)
+    assert_equal(my_calculator.subtract(-12), 10)
+  end
+
+  def test_multiply
+    my_calculator = Calculator.new
+    my_calculator.accumulator = 5
+    assert_equal(35, my_calculator.multiply(7))
+    assert_equal(105, my_calculator.multiply(3))
+  end
+
+  def test_divide
+    my_calculator = Calculator.new
+    my_calculator.accumulator = 5
+    assert_equal(2.5, my_calculator.divide(2))
+    assert_equal(-10, my_calculator.divide(-0.25), 4)
+  end
 end
